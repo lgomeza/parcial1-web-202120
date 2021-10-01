2bfc57393e07b6257b43fd5fbdc10a3b2957aa7f
Luis Gómez <l.gomeza@uniandes.edu.co>
2021-10-01 14:15:02 -0500

    Exam finished

--- public/script.js
+++ public/script.js
@@ -1,14 +1,15 @@
-const enterButton = document.getElementById('enter');
-const input = document.getElementById('inputText');
-const tableSection = document.getElementById('table-section');
-const table = document.getElementById('table');
-const tbody = document.getElementById('body-table');
-const message = document.getElementById('message');
+const enterButton = document.getElementById("enter");
+const input = document.getElementById("inputText");
+const table = document.getElementById("table");
+const tbody = document.getElementById("body-table");
+const message = document.getElementById("message");
 
-enterButton.addEventListener('click', (event) => {
-  //Implementar lógica del button submit
-  alert('Implementar lógica del button submit');
-  getresults(123);
+const host = "localhost";
+const port = "3000";
+
+enterButton.addEventListener("click", (event) => {
+  targetHeight = parseInt(input.value);
+  getresults(targetHeight);
   event.preventDefault();
 });
 
@@ -17,10 +18,45 @@ enterButton.addEventListener('click', (event) => {
  * @param {*} heightRef
  */
 async function getresults(heightRef) {
-  const resp = await fetch(`api?input=${heightRef}`);
-  const data = await resp.json();
-  console.log('data from back', data);
-  //printValues(data);
+  const resp = await fetch(`http://${host}:${port}/api/?input=${heightRef}`);
+  const rawData = await resp.json();
+  const data = rawData.playerList;
+  printValues(data);
+}
+
+function createTd(content) {
+  const newTd = document.createElement("td");
+  newTd.innerHTML = content;
+  return newTd;
+}
+
+function createRow(playerData, rowIndex) {
+  const playerMap = new Map(Object.entries(playerData));
+
+  const newRow = document.createElement("tr");
+  newRow.appendChild(createTd(rowIndex));
+  playerMap.forEach((attValue) => {
+    newRow.appendChild(createTd(attValue));
+  });
+
+  return newRow;
+}
+
+function printValues(data) {
+  removeAllChildNodes(tbody);
+  if(data.length === 0) {
+    message.innerHTML = "No matches found. Try again";
+    table.style.display = "none";
+  }
+  else{
+    table.style.display = "";
+    message.innerHTML = "";
+    let index = 1;
+    data.forEach(player => {
+      tbody.appendChild(createRow(player, index));
+      index += 1;
+    });
+  }
 }
 
 function removeAllChildNodes(parent) {
--- src/controller.js
+++ src/controller.js
@@ -1,8 +1,39 @@
-const { response, request } = require('express');
+const { response, request } = require("express");
+const axios = require("axios");
 
-const getPairsOfPlayers = async (req = request, resp = response, next) => {
+/**
+ * Receives a url and gets the data out of the fetched JSON response (uses axios)
+ * @param  {String} url The url where the needed data is stored
+ * @return {Object} Returns the data property of the response.
+ */
+async function get(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data.values;
+  } catch (error) {
+    return {};
+  }
+}
+
+function getFullName(player) {
+  return `${player["first_name"]} ${player["last_name"]}`;
+}
+
+const getPairsOfPlayers = async (req = request, resp = response) => {
   //Implementar logica aquí
-  return resp.json({ message: 'To be implemented' });
+  const data = await get("https://gist.githubusercontent.com/jhonatan89/bc554ec3ded15407609714e8b7b8f1c8/raw/5ab1e3e5b45e99aabcbbdd30f55d2ae5eafb9cbe/nba-players");
+  const solArray = [];
+  const targetValue = req.query.input;
+  for(let i = 0; i < data.length - 1; i++) {
+    for(let j = i+1; j < data.length; j++) {
+      const firstPossiblePlayer = data[i];
+      const secondPossiblePlayer = data[j];
+      if(parseInt(firstPossiblePlayer["h_in"]) + parseInt(secondPossiblePlayer["h_in"]) === parseInt(targetValue)) {
+        solArray.push({firstPlayer: `${getFullName(firstPossiblePlayer)}`, secondPlayer: `${getFullName(secondPossiblePlayer)}`});
+      }
+    }
+  }
+  return resp.json({ playerList: solArray });
 };
 
 module.exports = { getPairsOfPlayers };
